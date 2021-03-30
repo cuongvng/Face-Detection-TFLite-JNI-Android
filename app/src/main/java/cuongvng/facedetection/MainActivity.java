ab2be3c627489d78ef775490ef1d327c2f0d2054
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-03-30 16:03:28 +0700

    call detectJNI

--- app/src/main/java/cuongvng/facedetection/MainActivity.java
+++ app/src/main/java/cuongvng/facedetection/MainActivity.java
@@ -2,6 +2,7 @@ package cuongvng.facedetection;
 
 import android.Manifest;
 import android.app.Activity;
+import android.graphics.*;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.os.Bundle;
@@ -19,12 +20,25 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
+import com.google.flatbuffers.kotlin.ByteArrayKt;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.frame.Frame;
+
 public class MainActivity extends Activity implements CvCameraViewListener2 {
     private static final String TAG = "MainActivity";
     private static final int CAMERA_PERMISSION_REQUEST = 1;
+
     private final String MODEL_FILE = "centerface_w640_h480.tflite";
     private long detectorPointer = 0L;
-    private CameraBridgeViewBase mOpenCvCameraView;
+    private float heatmapThreshold = 0.5;
+    private float nmsThreshold = 0.3;
+    private final int nFaceInfo = 5;
+
+    private int frameWidth = 0;
+    private int frameHeight = 0;
+    private int rotationToUser = 0;
+    private final Paint _paint = Paint();
+//    private CameraBridgeViewBase mOpenCvCameraView;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -35,7 +49,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                 // Load native library after(!) OpenCV initialization
                 System.loadLibrary("native-lib");
 
-                mOpenCvCameraView.enableView();
+//                mOpenCvCameraView.enableView();
             } else {
                 super.onManagerConnected(status);
             }
@@ -54,80 +68,140 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                 new String[]{Manifest.permission.CAMERA},
                 CAMERA_PERMISSION_REQUEST
         );
-
-        setContentView(R.layout.activity_main);
-
-        mOpenCvCameraView = findViewById(R.id.main_surface);
-
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-
-        mOpenCvCameraView.setCvCameraViewListener(this);
-
         // Load model
         detectorPointer = loadDetectorJNI(this.getAssets(), MODEL_FILE);
+
+        setContentView(R.layout.activity_main);
+        CameraView cameraView = this.findViewById<CameraView>(R.id.camera);
+        cameraView.setLifecycleOwner(this);
+        cameraView.addFrameProcessor(frame -> {detectFaceNative(frame);});
+
+//        // init the paint for drawing the detections
+//        _paint.color = Color.RED;
+//        _paint.style = Paint.Style.STROKE;
+//        _paint.strokeWidth = 3f;
+//        _paint.textSize = 50f;
+//        _paint.textAlign = Paint.Align.LEFT;
+//
+//        // Set the detections drawings surface transparent
+//        surfaceView.setZOrderOnTop(true);
+//        surfaceView.holder.setFormat(PixelFormat.TRANSPARENT);
+//
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == CAMERA_PERMISSION_REQUEST) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                mOpenCvCameraView.setCameraPermissionGranted();
-            } else {
-                String message = "Camera permission was not granted";
-                Log.e(TAG, message);
-                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-            }
-        } else {
-            Log.e(TAG, "Unexpected permission request");
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        if (requestCode == CAMERA_PERMISSION_REQUEST) {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                mOpenCvCameraView.setCameraPermissionGranted();
+//            } else {
+//                String message = "Camera permission was not granted";
+//                Log.e(TAG, message);
+//                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+//            }
+//        } else {
+//            Log.e(TAG, "Unexpected permission request");
+//        }
+//    }
+
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        if (mOpenCvCameraView != null)
+//            mOpenCvCameraView.disableView();
+//    }
+
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        if (!OpenCVLoader.initDebug()) {
+//            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+//            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+//        } else {
+//            Log.d(TAG, "OpenCV library found inside package. Using it!");
+//            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+//        }
+//    }
+
+    private void detectFaceNative(Frame frame){
+        final long start = System.currentTimeMillis();
+        if (detectorPointer == 0L){
+            detectorPointer == loadDetectorJNI(this.getAssets(), MODEL_FILE);
         }
+
+        frameHeight = frame.getSize().getHeight();
+        frameWidth = frame.getSize().getWidth();
+        rotationToUser = frame.getRotationToUser();
+
+        float[] detections = this.detectJNI(
+                detectorPointer,
+                frame.getData(),
+                heatmapThreshold,
+                nmsThreshold,
+                frameWidth,
+                frameHeight,
+                rotationToUser
+                );
+
+        final long elapsed = System.currentTimeMillis() - start;
+        Log.i(TAG, "Detection elapsed time: ${elapsed}ms");
+
+//        val canvas = surfaceView.holder.lockCanvas();
+//        if (canvas != null) {
+//            canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.MULTIPLY)
+//            // Draw the detections, in our case there are only 3
+//            this.drawDetection(canvas, rotationToUser, detections, 0);
+//            this.drawDetection(canvas, rotationToUser, detections, 1);
+//            this.drawDetection(canvas, rotationToUser, detections, 2);
+//            surfaceView.holder.unlockCanvasAndPost(canvas);
+//        }
     }
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//        if (mOpenCvCameraView != null)
+//            mOpenCvCameraView.disableView();
+//    }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
+//    @Override
+//    public void onCameraViewStarted(int width, int height) {
+//    }
+//
+//    @Override
+//    public void onCameraViewStopped() {
+//    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-    }
+//    @Override
+//    public Mat onCameraFrame(CvCameraViewFrame frame) {
+//        // get current camera frame as OpenCV Mat object
+//        Mat mat = frame.rgba();
+//
+////        // native call to process current camera frame
+////        adaptiveThresholdFromJNI(mat.getNativeObjAddr());
+//
+//        // return processed frame for live preview
+//        return mat;
+//    }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-    }
-
-    @Override
-    public Mat onCameraFrame(CvCameraViewFrame frame) {
-        // get current camera frame as OpenCV Mat object
-        Mat mat = frame.gray();
-
-//        // native call to process current camera frame
-//        adaptiveThresholdFromJNI(mat.getNativeObjAddr());
-
-        // return processed frame for live preview
-        return mat;
-    }
-
-//    private native void adaptiveThresholdFromJNI(long mat);
+//    private void drawDetection(Canvas canvas, int rotation, float[] detections, int faceIndex){
+//        // Frame dimensions
+//        int w;
+//        int h;
+//        if (rotation == 0 || rotation == 180){
+//            w = frameWidth;
+//            h = frameHeight;
+//        }
+//        else{
+//            w = frameHeight;
+//            h = frameWidth;
+//        }
+//
+//        float scaleX =  / w;
+//
+//
+//    }
     private native long loadDetectorJNI(AssetManager assetManager, String filename);
+    private native float[] detectJNI(long detectorPtr, ByteArrayKt src,
+                                     float heatmapThreshold, float nmsThreshold,
+                                     int width, int heith, int rotation);
 }
