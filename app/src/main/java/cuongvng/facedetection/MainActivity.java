d3cacf2f008e1615d5cf241babfeea629206df0f
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-03-30 16:58:35 +0700

    app keeps stopping

--- app/src/main/java/cuongvng/facedetection/MainActivity.java
+++ app/src/main/java/cuongvng/facedetection/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.LifecycleOwner;
+
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
@@ -20,46 +22,51 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
-import com.google.flatbuffers.kotlin.ByteArrayKt;
+//import com.google.flatbuffers.kotlin.ByteArrayKt;
 import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.frame.Frame;
 
-public class MainActivity extends Activity implements CvCameraViewListener2 {
+public class MainActivity extends Activity{
     private static final String TAG = "MainActivity";
     private static final int CAMERA_PERMISSION_REQUEST = 1;
 
     private final String MODEL_FILE = "centerface_w640_h480.tflite";
     private long detectorPointer = 0L;
-    private float heatmapThreshold = 0.5;
-    private float nmsThreshold = 0.3;
+    private float heatmapThreshold = (float) 0.5;
+    private float nmsThreshold = (float) 0.3;
     private final int nFaceInfo = 5;
 
     private int frameWidth = 0;
     private int frameHeight = 0;
     private int rotationToUser = 0;
-    private final Paint _paint = Paint();
+    private Paint _paint = new Paint();
+    private SurfaceView surfaceView = new SurfaceView(this);
+    private CameraView cameraView = new CameraView(this);
+
 //    private CameraBridgeViewBase mOpenCvCameraView;
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            if (status == LoaderCallbackInterface.SUCCESS) {
-                Log.i(TAG, "OpenCV loaded successfully");
-
-                // Load native library after(!) OpenCV initialization
-                System.loadLibrary("native-lib");
-
-//                mOpenCvCameraView.enableView();
-            } else {
-                super.onManagerConnected(status);
-            }
-        }
-    };
+//    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+//        @Override
+//        public void onManagerConnected(int status) {
+//            if (status == LoaderCallbackInterface.SUCCESS) {
+//                Log.i(TAG, "OpenCV loaded successfully");
+//
+//                // Load native library after(!) OpenCV initialization
+//                System.loadLibrary("native-lib");
+//
+////                mOpenCvCameraView.enableView();
+//            } else {
+//                super.onManagerConnected(status);
+//            }
+//        }
+//    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
+
+        System.loadLibrary("native-lib");
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         // Permissions for Android 6+
@@ -72,21 +79,21 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         detectorPointer = loadDetectorJNI(this.getAssets(), MODEL_FILE);
 
         setContentView(R.layout.activity_main);
-        CameraView cameraView = this.findViewById<CameraView>(R.id.camera);
-        cameraView.setLifecycleOwner(this);
+        cameraView = this.findViewById(R.id.camera);
+        surfaceView = this.findViewById(R.id.surfaceView);
+
+        cameraView.setLifecycleOwner((LifecycleOwner) this);
         cameraView.addFrameProcessor(frame -> {detectFaceNative(frame);});
 
-//        // init the paint for drawing the detections
-//        _paint.color = Color.RED;
-//        _paint.style = Paint.Style.STROKE;
-//        _paint.strokeWidth = 3f;
-//        _paint.textSize = 50f;
-//        _paint.textAlign = Paint.Align.LEFT;
-//
-//        // Set the detections drawings surface transparent
-//        surfaceView.setZOrderOnTop(true);
-//        surfaceView.holder.setFormat(PixelFormat.TRANSPARENT);
-//
+        // init the paint for drawing the detections
+        _paint.setColor(Color.RED);
+        _paint.setStyle(Paint.Style.STROKE);
+        _paint.setStrokeWidth(3f);
+
+        // Set the detections drawings surface transparent
+        surfaceView.setZOrderOnTop(true);
+        surfaceView.getHolder().setFormat(PixelFormat.TRANSPARENT);
+
     }
 
 //    @Override
@@ -126,7 +133,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     private void detectFaceNative(Frame frame){
         final long start = System.currentTimeMillis();
         if (detectorPointer == 0L){
-            detectorPointer == loadDetectorJNI(this.getAssets(), MODEL_FILE);
+            detectorPointer = loadDetectorJNI(this.getAssets(), MODEL_FILE);
         }
 
         frameHeight = frame.getSize().getHeight();
@@ -146,15 +153,15 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         final long elapsed = System.currentTimeMillis() - start;
         Log.i(TAG, "Detection elapsed time: ${elapsed}ms");
 
-//        val canvas = surfaceView.holder.lockCanvas();
-//        if (canvas != null) {
-//            canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.MULTIPLY)
-//            // Draw the detections, in our case there are only 3
-//            this.drawDetection(canvas, rotationToUser, detections, 0);
-//            this.drawDetection(canvas, rotationToUser, detections, 1);
-//            this.drawDetection(canvas, rotationToUser, detections, 2);
-//            surfaceView.holder.unlockCanvasAndPost(canvas);
-//        }
+        Canvas canvas = surfaceView.getHolder().lockCanvas();
+        if (canvas != null) {
+            canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.MULTIPLY);
+            // Draw the detections, in our case there are only 3
+            this.drawDetection(canvas, rotationToUser, detections, 0);
+            this.drawDetection(canvas, rotationToUser, detections, 1);
+            this.drawDetection(canvas, rotationToUser, detections, 2);
+            surfaceView.getHolder().unlockCanvasAndPost(canvas);
+        }
     }
 //    @Override
 //    public void onDestroy() {
@@ -183,25 +190,40 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
 //        return mat;
 //    }
 
-//    private void drawDetection(Canvas canvas, int rotation, float[] detections, int faceIndex){
-//        // Frame dimensions
-//        int w;
-//        int h;
-//        if (rotation == 0 || rotation == 180){
-//            w = frameWidth;
-//            h = frameHeight;
-//        }
-//        else{
-//            w = frameHeight;
-//            h = frameWidth;
-//        }
-//
-//        float scaleX =  / w;
-//
-//
-//    }
+    private void drawDetection(Canvas canvas, int rotation, float[] detections, int idx){
+        // Frame dimensions
+        int w;
+        int h;
+        if (rotation == 0 || rotation == 180){
+            w = frameWidth;
+            h = frameHeight;
+        }
+        else{
+            w = frameHeight;
+            h = frameWidth;
+        }
+
+        float scaleX =  (float) cameraView.getWidth() / w;
+        float scaleY = (float) cameraView.getHeight() / h;
+        float xOffset =  (float) cameraView.getLeft();
+        float yOffset = (float) cameraView.getTop();
+
+        float x1 = xOffset + detections[idx*nFaceInfo] * scaleX;
+        float y1 = yOffset + detections[idx*nFaceInfo + 1] * scaleY;
+        float x2 = xOffset + detections[idx*nFaceInfo + 2] * scaleX;
+        float y2 = yOffset + detections[idx*nFaceInfo + 3] * scaleY;
+
+        Path p = new Path();
+        p.moveTo(x1, y1);
+        p.lineTo(x1, y2);
+        p.lineTo(x2, y2);
+        p.lineTo(x2, y1);
+        p.lineTo(x1, y1);
+
+        canvas.drawPath(p, _paint);
+    }
     private native long loadDetectorJNI(AssetManager assetManager, String filename);
-    private native float[] detectJNI(long detectorPtr, ByteArrayKt src,
+    private native float[] detectJNI(long detectorPtr, byte[] src,
                                      float heatmapThreshold, float nmsThreshold,
                                      int width, int heith, int rotation);
 }
